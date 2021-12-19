c49da49cc20e3e6ce6b722c00b5398f5c61e01da
andreyekaa <aemakhaev@miem.hse.ru>
2021-12-19 21:26:01 +0300

    CHANGED WORKING TESTS

--- test_snakey.py
+++ test_snakey.py
@@ -1,44 +1,48 @@
-#from _typeshed import Self
 import unittest
 import main
-import pytest
+import pygame
+import unittest.mock
+
 
 class TestGames(unittest.TestCase):
-    def test_exit_tree(self):
-        main.collide_with_tree
-        assert main.exit_from_game
+    def test_button_click(self):
+        dx, dy = 0, 0
+        key = {pygame.K_UP: True, pygame.K_w: False, pygame.K_DOWN: False, pygame.K_s: False,
+                pygame.K_LEFT: False, pygame.K_a: False,  pygame.K_RIGHT: False, pygame.K_d: False}
+        self.assertEquals(main.buttons_click(dx, dy, key), (0, -50))
 
-    def test_exit(self):
-        main.exit_from_game
-        assert True
-
-    def test_eating_cookie(self):
-        cookiex = 1
-        cookiey = 1
-        main.eating_cookie()
-        legth = leng = 10
-        scor = score = 10
-        spee = v = 16
-
-        leng = main.eating_cookie
-        score = main.eating_cookie
-        v = main.eating_cookie
-
-        assert (legth != leng) and (scor != score) and (spee != v)
+        key[pygame.K_UP] = False
+        key[pygame.K_DOWN] = True
+        self.assertEquals(main.buttons_click(dx, dy, key), (0, 50))
         
+        key[pygame.K_DOWN] = False 
+        key[pygame.K_a] = True
+        self.assertEquals(main.buttons_click(dx, dy, key), (-50, 0))
+
+        key[pygame.K_a] = False 
+        key[pygame.K_d] = True
+        self.assertEquals(main.buttons_click(dx, dy, key), (50, 0))
 
     def test_record_less(self):
         main.record
         score = 10
         sc = 20
-        self.assertEqual(main.record(score, sc), sc)
+        self.assertEquals(main.record(score, sc), sc)
 
     def test_record_more(self):
         main.record
         score = 24
         sc = 20
-        self.assertEqual(main.record(score, sc), sc)
+        self.assertEqual(main.record(score, sc), score)
 
-
-
-
+    def test_colliding_with_walls(self):
+        '''Удар со стенами.'''
+        self.assertTrue(main.colliding_with_walls(-10, 10, 20, 20))  
+        self.assertTrue(main.colliding_with_walls(10,-1, 20, 20)) 
+        self.assertTrue(main.colliding_with_walls(21, 10, 20, 20))  
+        self.assertTrue(main.colliding_with_walls(10,21, 20, 20)) 
+        '''Нет удара со стенами'''
+        self.assertFalse(main.colliding_with_walls(19, 10, 20, 20))  
+        self.assertFalse(main.colliding_with_walls(10,19, 20, 20)) 
+                    
+         
